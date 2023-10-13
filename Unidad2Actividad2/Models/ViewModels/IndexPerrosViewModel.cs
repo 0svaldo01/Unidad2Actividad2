5ed67eff3bd27ce17f26e2279f1da290a12283c4
VICTOR OSVALDO RODRIGUEZ HIDALGO <201G0283@rcarbonifera.tecnm.mx>
2023-10-13 00:12:22 -0600

    queee? esta fallando?

---
+++ Unidad2Actividad2/Models/ViewModels/IndexPerrosViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Unidad2Actividad2.Models.ViewModels
+{
+    public class IndexPerrosViewModel
+    {
+        public int Id { get; set; }
+        public string NombreRaza { get; set; } = null!;
+
+    }
+}
